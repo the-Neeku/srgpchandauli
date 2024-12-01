b843ba7366e706af69922851b893df9c9b857015
the-Neeku <mauryaneeku718@gmail.com>
2024-12-02 04:02:41 +0530

    first commit

--- app.py
+++ app.py
@@ -4,23 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('templates/MAIN.html')
+    return render_template('MAIN.html')
 
 @app.route('/facility')
 def facility():
-    return render_template('templates/FACILITY.html')
+    return render_template('FACILITY.html')
 
 @app.route('/admission')
 def admission():
-    return render_template('templates/ADDMISION.html')
+    return render_template('ADDMISION.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('templates/CONTACT.html')
+    return render_template('CONTACT.html')
 
 @app.route('/about')
 def about():
-    return render_template('templates/ABOUT.html')
+    return render_template('ABOUT.html')
 
 if __name__ == '_main_': 
     app.run(debug=True)
