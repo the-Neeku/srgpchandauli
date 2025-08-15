385b5c698e7eaa7d3fb96722bb5858df2682f18d
Neeraj Kumar Maurya <mauryaneeku718@gmail.com>
2025-08-15 19:48:22 +0530

    Update settings.py

--- SRGP/settings.py
+++ SRGP/settings.py
@@ -69,8 +69,9 @@ DATABASES = {
 # ---------------------
 # Static & Media Files
 # ---------------------
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
