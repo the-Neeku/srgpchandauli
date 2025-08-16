288a3b015cd31c227975ebfac06896eb0a33b974
Neeraj Kumar Maurya <mauryaneeku718@gmail.com>
2025-08-16 20:15:00 +0530

    settings.py updated successfully...

--- SRGP/settings.py
+++ SRGP/settings.py
@@ -17,6 +17,9 @@ ALLOWED_HOSTS = [
 # Application definition
 INSTALLED_APPS = [
     'MAIN_APP',
+    'filebrowser',
+    'grappelli',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
